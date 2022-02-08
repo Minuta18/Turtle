5d0e2db8191eea69c17ab516832c7725d4d5ce80
GitHubik1 <ig.samsonov10@yandex.ru>
2022-02-08 17:30:40 +0300

    Version 1.0

---
+++ code/turtlePas.pas
@@ -0,0 +1,110 @@
+﻿unit turtlePas;
+
+interface
+
+  uses GraphABC;
+
+  type
+    ///Turtle - класс черепашки
+    turtle = class
+  
+    public
+    
+      ///Координата X
+      x: real;
+      ///Координата Y
+      y: real;
+      ///Угол
+      angle: real;
+      ///Опущено ли перо
+      isPenDown := false;
+      ///Системное
+      superFastMode := false;
+    
+      constructor Create(x, y: integer);
+      begin
+        self.x := x;
+        self.y := y;
+      end;
+    
+      ///Активирет "Супер-быстрый" рехжим
+      procedure ActiveSuperFastMode();
+      begin
+        superFastMode := true;
+        LockDrawing;
+      end;
+      
+      ///Максимизирует окно
+      procedure fullWindow();
+      begin
+        Window.Maximize;
+      end;
+      
+      ///Рендеринг черепашки в супер-быстром режиме
+      procedure update();
+      begin
+        if superFastMode then Redraw;
+      end;
+    
+      ///Устанавливает позицию черепашки
+      procedure setPos(x, y: integer);
+      begin
+        self.x := x;
+        self.y := y;
+      end;
+      
+      ///Опускает перо
+      procedure penDown();
+      begin
+        self.isPenDown := true;
+      end;
+      
+      ///Поднимает перо
+      procedure penUp();
+      begin
+        self.isPenDown := false;
+      end;
+      
+      ///Устанавливает Угол
+      procedure setAngle(angle: integer);
+      begin
+        self.angle := DegToRad(angle mod 360);
+      end;
+      
+      ///Поворачивает черепашку на угол
+      procedure newAngle(angle: integer);
+      begin
+        self.angle += DegToRad(angle mod 360);
+      end;
+      
+      ///Очищает всё
+      procedure clearAll();
+      begin
+        Window.Clear;
+      end;
+      
+      ///Изменяет толщину пера
+      procedure setPenWidth(n: integer);
+      begin
+        SetPenWidth(n);
+      end;
+      
+      ///Двигает черепашку на расстояние n
+      procedure go(n: integer);
+      begin
+        if(self.isPenDown)then begin
+          Line(Trunc(self.x), Trunc(self.y), Trunc(self.x + n * Cos(self.angle)), Trunc(self.y + n * Sin(self.angle)));
+          self.x := self.x + n * Cos(self.angle);
+          self.y := self.y + n * Sin(self.angle);
+        end else begin
+          self.x := self.x + n * Cos(self.angle);
+          self.y := self.y + n * Sin(self.angle);
+        end;
+        if not superFastMode then Sleep(50);
+      end;
+  
+    end;
+
+implementation
+
+end.
---
+++ examples/example2.pas
@@ -0,0 +1,11 @@
+﻿uses turtlePas, graphABC;
+
+var tur := new turtle(200, 100);
+
+begin
+  for var i := 0 to 180 do begin
+    tur.setAngle(i * 2);
+    tur.go(2);
+    Arc(Trunc(tur.x), Trunc(tur.y), 40, 0, 360);
+  end;
+end.
