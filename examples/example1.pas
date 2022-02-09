81dbb6f642ea4ccac7d1084292965dbc61aff1ab
GitHubik1 <ig.samsonov10@yandex.ru>
2022-02-09 07:56:09 +0300

    More examples

---
+++ examples/example1.pas
@@ -0,0 +1,15 @@
+﻿uses turtlePas, graphABC;
+
+var tur := new turtle(200, 100);
+
+begin
+  tur.setAngle(34);
+  tur.penDown();
+  tur.go(100);
+  tur.penUp();
+  tur.setAngle(0);
+  tur.go(90);
+  tur.setAngle(78);
+  tur.penDown();
+  tur.go(200);
+end.
